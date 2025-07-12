77f6be9cf8c505eeb4ec163a6d94ef34232ce9d4
Iftikhar Shams Niloy <niloyshams21@gmail.com>
2025-07-12 23:49:37 +0600

    File updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,19 +49,10 @@ dependencies {
 
     // Dependencies for ROOM
     dependencies {
-        val room_version = "2.7.2"
-
+        val room_version = "2.5.0"
         implementation("androidx.room:room-runtime:$room_version")
-
-        // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
-        // See Add the KSP plugin to your project
         ksp("androidx.room:room-compiler:$room_version")
-
-        // If this project only uses Java source, use the Java annotationProcessor
-        // No additional plugins are necessary
         annotationProcessor("androidx.room:room-compiler:$room_version")
-
-        // optional - Kotlin Extensions and Coroutines support for Room
         implementation("androidx.room:room-ktx:$room_version")
     }
 
